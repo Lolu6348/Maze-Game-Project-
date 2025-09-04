43d8580f4c5bdfa3470b84ccdf424938b7ff17b1
Lolu <Lolu@MacBook-Air.local>
2025-09-04 14:48:36 -0500

    Added enemy traps and an endpopint to the game

--- main.cpp
+++ main.cpp
@@ -1,55 +1,114 @@
 #include <iostream>
-#include <conio.h> // For _getch()
+#include <ctime>
 
-const int width = 10;
-const int height = 10;
-char maze[height][width] = {
-    {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-    {'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'},
-    {'#', ' ', '#', ' ', '#', ' ', '#', '#', ' ', '#'},
-    {'#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#'},
-    {'#', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#'},
-    {'#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#'},
-    {'#', '#', '#', '#', ' ', '#', ' ', '#', ' ', '#'},
-    {'#', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'},
-    {'#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
-    {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+class Maze {
+private:
+    const int width = 10;
+    const int height = 10;
+    char layout[10][10] = {
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+        {'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', '#', ' ', '#', ' ', '#', '#', ' ', '#'},
+        {'#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#'},
+        {'#', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#'},
+        {'#', '#', '#', '#', ' ', '#', ' ', '#', ' ', '#'},
+        {'#', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', '#', ' ', ' ', 'E', '#'},
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+    };
+
+public:
+    void draw(int playerX, int playerY) {
+        system("clear");  // use "cls" on Windows
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (i == playerY && j == playerX)
+                    std::cout << 'P';
+                else
+                    std::cout << layout[i][j];
+            }
+            std::cout << '\n';
+        }
+    }
+
+    bool isWalkable(int x, int y) {
+        return layout[y][x] == ' ' || layout[y][x] == 'T' || layout[y][x] == 'E';
+    }
+
+    bool isTrap(int x, int y) {
+        return layout[y][x] == 'T';
+    }
+
+    bool isExit(int x, int y) {
+        return layout[y][x] == 'E';
+    }
+
+    void placeTrap(int x, int y) {
+        if (layout[y][x] == ' ')
+            layout[y][x] = 'T';
+    }
+
+    bool isInBounds(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
 };
 
-int playerX = 1, playerY = 1;
+class Game {
+private:
+    Maze maze;
+    int playerX = 1, playerY = 1;
+    int moveCount = 0;
+    std::clock_t startTime;
 
-void drawMaze() {
-    system("cls");
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (i == playerY && j == playerX)
-                std::cout << 'P';
-            else
-                std::cout << maze[i][j];
+public:
+    void start() {
+        maze.placeTrap(5, 3); // Place a trap
+        maze.placeTrap(7, 4);
+        startTime = std::clock();
+
+        while (true) {
+            maze.draw(playerX, playerY);
+            std::cout << "\nMoves: " << moveCount << "\nUse WASD to move: ";
+            char input;
+            std::cin >> input;
+            movePlayer(tolower(input));
+
+            if (maze.isExit(playerX, playerY)) {
+                double duration = (std::clock() - startTime) / (double)CLOCKS_PER_SEC;
+                std::cout << "\n🎉 You escaped the maze in " << moveCount << " moves and "
+                          << duration << " seconds!\n";
+                break;
+            }
+
+            if (maze.isTrap(playerX, playerY)) {
+                std::cout << "\n💥 You stepped on a trap! Game Over.\n";
+                break;
+            }
         }
-        std::cout << std::endl;
     }
-}
 
-void movePlayer(char direction) {
-    int newX = playerX, newY = playerY;
-    switch (direction) {
-    case 'w': newY--; break;
-    case 's': newY++; break;
-    case 'a': newX--; break;
-    case 'd': newX++; break;
+private:
+    void movePlayer(char direction) {
+        int newX = playerX, newY = playerY;
+        switch (direction) {
+            case 'w': newY--; break;
+            case 's': newY++; break;
+            case 'a': newX--; break;
+            case 'd': newX++; break;
+            default: return;
+        }
+
+        if (maze.isInBounds(newX, newY) && maze.isWalkable(newX, newY)) {
+            playerX = newX;
+            playerY = newY;
+            moveCount++;
+        }
     }
-    if (maze[newY][newX] == ' ') {
-        playerX = newX;
-        playerY = newY;
-    }
-}
+};
 
 int main() {
-    while (true) {
-        drawMaze();
-        char input = _getch();
-        movePlayer(input);
-    }
+    Game mazeGame;
+    mazeGame.start();
     return 0;
 }
